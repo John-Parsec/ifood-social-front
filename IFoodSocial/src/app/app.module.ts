8a93661a48264365583d8dfee965307411b29982
João Manoel <jmaoliveira.cic@uesc.br>
2024-06-03 20:50:38 -0300

    add arquivos iniciais

---
+++ IFoodSocial/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ClienteModule } from './modules/cliente/cliente.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ClienteModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
