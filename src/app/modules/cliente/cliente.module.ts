9da853b5503bb7823de0082811776310ef3eeb66
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-06-11 09:48:07 -0300

    att navbar design

--- src/app/modules/cliente/cliente.module.ts
+++ src/app/modules/cliente/cliente.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from "@angular/common";
 import { HomeComponent } from "./components/home/home.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { MenubarModule } from "primeng/menubar";
-import { StoreListComponent } from './components/store-list/store-list.component';
+import { ToolbarModule } from "primeng/toolbar";
+import { StoreListComponent } from "./components/store-list/store-list.component";
+import { InputTextModule } from "primeng/inputtext";
 
 @NgModule({
-  declarations: [HomeComponent, NavbarComponent, FooterComponent, StoreListComponent],
-  imports: [CommonModule, MenubarModule],
+  declarations: [
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    StoreListComponent,
+  ],
+  imports: [CommonModule, ToolbarModule, InputTextModule],
   exports: [HomeComponent, NavbarComponent, FooterComponent],
 })
 export class ClienteModule {}
--- src/app/modules/cliente/components/navbar/navbar.component.ts
+++ src/app/modules/cliente/components/navbar/navbar.component.ts
@@ -7,58 +7,11 @@ import { MenuItem } from "primeng/api";
   styleUrl: "./navbar.component.css",
 })
 export class NavbarComponent {
-  items: MenuItem[] = [];
+  toReceits() {
+    console.log("toReceits");
+  }
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: "Home",
-        icon: "pi pi-home",
-      },
-      {
-        label: "Features",
-        icon: "pi pi-star",
-      },
-      {
-        label: "Projects",
-        icon: "pi pi-search",
-        items: [
-          {
-            label: "Core",
-            icon: "pi pi-bolt",
-            shortcut: "⌘+S",
-          },
-          {
-            label: "Blocks",
-            icon: "pi pi-server",
-            shortcut: "⌘+B",
-          },
-          {
-            label: "UI Kit",
-            icon: "pi pi-pencil",
-            shortcut: "⌘+U",
-          },
-          {
-            separator: true,
-          },
-          {
-            label: "Templates",
-            icon: "pi pi-palette",
-            items: [
-              {
-                label: "Apollo",
-                icon: "pi pi-palette",
-                badge: "2",
-              },
-              {
-                label: "Ultima",
-                icon: "pi pi-palette",
-                badge: "3",
-              },
-            ],
-          },
-        ],
-      },
-    ];
+  toProfile() {
+    console.log("toProfile");
   }
 }
