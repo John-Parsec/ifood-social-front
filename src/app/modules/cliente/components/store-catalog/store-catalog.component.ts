d5e00a131faf3bd013428d25a3eb7c87ee2336d3
João Manoel <jmaoliveira.cic@uesc.br>
2024-06-15 22:01:33 -0300

    fix

--- src/app/modules/cliente/components/store-catalog/store-catalog.component.ts
+++ src/app/modules/cliente/components/store-catalog/store-catalog.component.ts
@@ -16,21 +16,27 @@ export class StoreCatalogComponent {
       name: 'Product 1',
       price: 100,
       image: '',
-      rating: 4
+      rating: 4,
+      category: 'Category 1',
+      description: 'Description 1'
     },
     {
       id: '2',
       name: 'Product 2',
       price: 200,
       image: '',
-      rating: 5
+      rating: 5,
+      category: 'Category 1',
+      description: 'Description 2'
     },
     {
       id: '3',
       name: 'Product 3',
       price: 300,
       image: '',
-      rating: 0
+      rating: 0,
+      category: 'Category 2',
+      description: 'Description 4'
     }
   ];
 
--- src/app/modules/cliente/models/product.ts
+++ src/app/modules/cliente/models/product.ts
@@ -4,5 +4,6 @@ export interface Product {
     price?: number,
     image?: string,
     rating?: number,
+    category?: string,
     description?: string;
 }
