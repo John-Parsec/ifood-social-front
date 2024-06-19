bc72aa5825a729ce61d5f36745c5063094952a4e
João Manoel <jmaoliveira.cic@uesc.br>
2024-06-19 16:12:07 -0300

    feat: Atualizar estilos do componente de carrinho e adicionar informações da loja

--- src/app/modules/cliente/components/cart/cart.component.ts
+++ src/app/modules/cliente/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Product } from '../../models/product';
+import { Store } from '../../models/store';
 
 @Component({
   selector: 'app-cart',
@@ -8,6 +9,15 @@ import { Product } from '../../models/product';
 })
 export class CartComponent {
   imgPath = '../../../../../assets/imgs/icon-image-not-found-free-vector.jpg';
+
+  store: Store = {
+    id : 1,
+    name: 'Loja 1',
+    description: 'Descrição da loja 1',
+    pathImage: this.imgPath,
+    deliveryFee: 10,
+    rating: 4,
+  };
 
   products: Product[] = [
     {
@@ -48,10 +58,6 @@ export class CartComponent {
     if (product.quantity && product.quantity > 0) {
       product.quantity--;
     }
-
-    if (product.quantity === 0) {
-      this.removeItem(product);
-    }
   }
 
   plusQtde(product: Product) {
@@ -69,7 +75,7 @@ export class CartComponent {
       return 0;
     }
     
-    return this.products.reduce((acc, product) => acc + product.price * (product.quantity ?? 0), 0);
+    return this.products.reduce((acc, product) => acc + product.price * (product.quantity ?? 0), 0) + this.store.deliveryFee;
   }
 
   detalharProduto(product: Product) {
