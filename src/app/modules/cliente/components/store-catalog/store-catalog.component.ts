cd35d95541592149f54331e9e577d0011d5de30f
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-06-19 18:45:55 -0300

    fix category bar

--- src/app/modules/cliente/components/store-catalog/store-catalog.component.ts
+++ src/app/modules/cliente/components/store-catalog/store-catalog.component.ts
@@ -1,16 +1,18 @@
 import { DatabaseService } from "./../../services/database.service";
-import { Component } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Product } from "../../models/product";
 import { Store } from "../../models/store";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-store-catalog",
   templateUrl: "./store-catalog.component.html",
   styleUrl: "./store-catalog.component.css",
 })
-export class StoreCatalogComponent {
-  imgPath = "../../../../../assets/imgs/icon-image-not-found-free-vector.jpg";
+export class StoreCatalogComponent implements OnInit {
+  lojaPath = "../../../../../assets/imgs/logo_padrao_loja.png";
+  produtoPath = "../../../../../assets/imgs/foto_padrao_produto.png";
 
   id: number;
   store: Store = {
@@ -22,48 +24,12 @@ export class StoreCatalogComponent {
     deliveryFee: 0,
   };
 
-  products: Product[] = [
-    {
-      id: "1",
-      name: "Produto 1",
-      price: 100,
-      image: this.imgPath,
-      rating: 4,
-      category: "Categoria 1",
-      description: "Descrição 1",
-    },
-    {
-      id: "2",
-      name: "Produto 2",
-      price: 200,
-      image: this.imgPath,
-      rating: 5,
-      category: "Categoria 1",
-      description:
-        "Bem-vindo ao mundo dos sanduíches Subway, onde a criatividade culinária encontra a conveniência! Nossa categoria de sanduíches oferece uma variedade de opções para todos os gostos, desde clássicos até criações personalizadas. Escolha o seu tipo de pão, proteína, vegetais, queijos e molhos para montar o sanduíche perfeito. Com ingredientes frescos e saborosos, os nossos sanduíches são uma escolha deliciosa e satisfatória para qualquer ocasião.",
-    },
-    {
-      id: "3",
-      name: "Produto 3",
-      price: 300,
-      image: this.imgPath,
-      rating: 0,
-      category: "Categoria 2",
-      description:
-        "Bem-vindo ao mundo dos sanduíches Subway, onde a criatividade culinária encontra a conveniência! Nossa categoria de sanduíches oferece uma variedade de opções para todos os gostos, desde clássicos até criações personalizadas. Escolha o seu tipo de pão, proteína, vegetais, queijos e molhos para montar o sanduíche perfeito. Com ingredientes frescos e saborosos, os nossos sanduíches são uma escolha deliciosa e satisfatória para qualquer ocasião.",
-    },
-  ];
+  products: Product[] = [];
 
   productsFiltered: Product[] = this.products;
   layout: "list" | "grid" = "grid";
 
-  categorias = [
-    { id: "0", name: "Tudo" },
-    { id: "1", name: "Categoria 1" },
-    { id: "2", name: "Categoria 2" },
-    { id: "3", name: "Categoria 3" },
-  ];
-
+  categorias: any[] = [{ id: "0", name: "Tudo" }];
   idCategoriaSelecionada = "0";
 
   constructor(
@@ -74,22 +40,33 @@ export class StoreCatalogComponent {
     console.log(this.id);
 
     this.database.getStoreById(this.id).subscribe((store) => {
-      console.log(store);
       this.store.id = store.cod_cod_empreedimento;
       this.store.name = store.dcr_nome_fantasia;
       this.store.description = `Descricao do empreendimento '${store.dcr_nome_fantasia}'`;
-      this.store.pathImage = this.imgPath;
+      this.store.pathImage = this.lojaPath;
     });
+  }
 
+  ngOnInit() {
     this.database.getCategoriesByStore(this.id).subscribe((categories) => {
-      // this.categorias = [
-      //   { id: "0", name: "Tudo" },
-      //   ...categories.map((category) => ({
-      //     id: category.cod_categoria,
-      //     name: category.dcr_nome,
-      //   })),
-      // ];
-      console.log(categories);
+      categories.forEach((element: any) => {
+        this.categorias = [
+          ...this.categorias,
+          { id: element.cod_categoria.toString(), name: element.dcr_categoria },
+        ];
+        element.produtos.forEach((product: any) => {
+          this.products.push({
+            id: product.cod_produto,
+            name: product.dcr_produto,
+            price: product.vlr_produto,
+            image: this.produtoPath,
+            rating: 5,
+            category: element.dcr_categoria,
+            description: `Descrição do produto '${product.dcr_produto}'`,
+          });
+        });
+      });
+      console.log(this.categorias);
     });
   }
 
