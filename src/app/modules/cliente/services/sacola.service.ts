9d881215c1bb97f34b7e10d3806542c4eb55c26e
João Manoel <jmaoliveira.cic@uesc.br>
2024-06-19 23:22:02 -0300

    feat: Atualizar serviço de sacola para permitir adicionar e remover quantidades de produtos

--- src/app/modules/cliente/services/sacola.service.ts
+++ src/app/modules/cliente/services/sacola.service.ts
@@ -12,7 +12,33 @@ export class SacolaService {
   constructor() { }
 
   adicionarProduto(produto: Product, observacao: string) {
+    if (this.produtos.some(p => p.id === produto.id)) {
+      this.produtos.map(p => {
+        if (p.id === produto.id) {
+          p.quantity? p.quantity++ : p.quantity = 1;
+        }
+      });
+      return;
+    }
+
+    produto.quantity = 1;
     this.produtos.push(produto);
     this.observacoes.push(observacao);
+  }
+
+  minusQtde(product: Product) {
+    this.produtos.map(p => {
+      if (p.id === product.id && p.quantity && p.quantity > 0) {
+        p.quantity--;
+      }
+    });
+  }
+
+  plusQtde(product: Product) {
+    this.produtos.map(p => {
+      if (p.id === product.id && p.quantity) {
+        p.quantity++;
+      }
+    });
   }
 }
